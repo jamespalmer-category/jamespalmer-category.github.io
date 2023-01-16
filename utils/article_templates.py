0c31d029013f1735c9f4c444b13d5b392c4468cb
James Palmer <jamesgpalmer@live.co.uk>
2023-01-16 20:29:47 +0000

    Added article on Sunak's maths plans + utils folder
    Utils folder here is to try and streamline getting articles into HTML format.

---
+++ utils/article_templates.py
@@ -0,0 +1,26 @@
+from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+from typing import List, Dict
+import re
+
+def article_preprocessing(filepath : str, article_name : str) -> List:
+    with open(filepath, 'r') as file:
+        article_string = file.read()
+        article_list = re.split('\n', article_string)
+    
+    return {'title': article_name, 'paragraphs' : article_list}
+
+def render_article(template_path : str, inputs : Dict, output_path : str) -> None:
+    """
+    inputs: the Dictionaries above
+    output_path: starts with current working directory - so you can guide it to whichever page you want it to be in!
+    """
+    file_loader = FileSystemLoader(Path(template_path).parent)
+    env = Environment(loader = file_loader)
+    article_html = env.get_template(Path(template_path).name).render(article_dict = inputs)
+    with open(output_path, 'w') as file:
+        file.write(article_html)
+
+if __name__ == "__main__":
+    input_ = article_preprocessing(Path(__file__).parent.joinpath('articles_to_process', 'Maths_Until_18_and_beyond.txt'), 'Maths Until 18 and Beyond')
+    render_article(str(Path(__file__).parent.joinpath('jinja_templates', 'article_template.txt')), input_, str(Path(__file__).parent.joinpath('rendered_templates', 'Maths_Until_18_and_Beyond.html')))
